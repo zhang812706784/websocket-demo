d8022505908afa7cb65ec3d03ff9f5b2929ad419
DESKTOP-ESNQ2LH\86177 <812706784@qq.com>
2021-09-25 02:05:08 +0800

    demo1更新

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # websocket-demo
-websocket案例   H5-websocket 
-后台nodeJs  
-中间件koa
-后台websocket库 ws
+websocket案例(H5-websocket)
+
+后台nodeJs 
+安装
+    ws  npm install ws
+    koa
+    koa-router 
+    
+    # nunjucks(nodeJS一个html模板)  npm install nunjucks 没用到
+
+
+demo1:
+    简单的websocket创建及后端交互
+demo2:
+    简易聊天室
+
+参考文档：
+https://zhuanlan.zhihu.com/p/149680021
+https://juejin.cn/post/6844903544978407431
+
+杀进程：
+netstat -aon | findstr "8080"
+taskkill /pid 4456 /F
---
+++ demo1/server/app.js
@@ -0,0 +1,55 @@
+// https://www.npmjs.com/package/ws  nodeJs一个ws库 用于websocket
+const WebSocket = require('ws');
+const path = require("path")
+const Koa = require('koa');
+const router = require('koa-router')();
+const views = require('koa-views')
+const serve = require('koa-static')
+
+const app = new Koa();
+
+const httpServer = app.listen(30002);
+
+// begin websocket start
+// 引用server类
+const WebSocketServer = WebSocket.Server;
+
+// 在30001端口上打开一个WebSocket Server
+let wss = new WebSocketServer({
+    server: httpServer
+})
+console.log(path.join(__dirname + '/../view'))
+// 这地方有个bug
+app.use(views(path.join(__dirname + '/../view'),
+    {
+        extension: 'html'
+    }
+)).use(router.routes());
+
+// 前端websocket发送连接请求 走这里
+wss.on('connection', function (ws) {
+    // ws websocket实例
+    console.log(`[SERVER] connection()`);
+    ws.on('message', function (message) {
+        console.log(`[SERVER] Received: ${message}`);
+        ws.send(`ECHO: ${message}`, (err) => {
+            if (err) {
+                console.log(`[SERVER] error: ${err}`);
+            }
+        });
+    })
+});
+
+// end websocket start
+
+// url地址请求页面
+router.get('/simple-demo', async (ctx, next)=>{
+    await ctx.render('simple-demo');
+})
+console.log(__dirname)
+
+
+
+
+
+
